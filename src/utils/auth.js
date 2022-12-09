8586b38a4cfe218a430ef23b0e59b76eb7ac9ce2
rofa <zainrofa29@gmail.com>
2022-12-10 05:42:21 +0700

    add auth API

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const BaseUrl = process.env.BACKEND_URL
+
+export const register = body => {
+  const URL = `${BaseUrl}/api/auths/register`;
+  // console.log('util', URL);
+  return axios.post(URL, body);
+}
+
+export const login = body => {
+  const URL = `${BaseUrl}api/auths/login`;
+  // console.log('util', body);
+  return axios.post(URL, body);
+};
+
+export const forgot = body => {
+  const URL = `${BaseUrl}api/auths/forgot-password`;
+  // console.log('util', body);
+  return axios.patch(URL, body);
+};
+
+export const reset = body => {
+  const URL = `${BaseUrl}api/auths/reset-password`;
+  // console.log('util', body);
+  return axios.patch(URL, body);
+};
