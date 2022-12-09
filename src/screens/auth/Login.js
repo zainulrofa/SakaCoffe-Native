eb9e95fa4fab2e9c1d25454649665e5aa42843cf
rofa <zainrofa29@gmail.com>
2022-12-10 05:41:35 +0700

    add login implementation

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,32 +1,68 @@
-import { View, Text, ImageBackground, TouchableOpacity, Image } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity, Image, ToastAndroid, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 
 import styles from "../../styles/Login";
 import Input from "../../components/Input";
 import google from "../../assets/images/google.png";
+import { useDispatch, useSelector } from 'react-redux';
+import authAction from '../../redux/actions/auth';
+import { useNavigation } from '@react-navigation/native';
 
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const navigation = useNavigation()
+    const auth = useSelector(state => state.auth)
+    console.log(auth.userData)
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    })
+
+    const onChangeHandler = (text, type) => {
+        setForm(form => ({ ...form, [type]: text }));
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const loginSuccess = () => {
+            ToastAndroid.showWithGravity(
+                `Welcome ${form.email}, login successfully`,
+                ToastAndroid.SHORT,
+                ToastAndroid.TOP,
+            )
+            navigation.navigate('Home')
+        }
+        const loginError = (error) => {
+            ToastAndroid.showWithGravity(
+                `${error}`,
+                ToastAndroid.SHORT,
+                ToastAndroid.TOP,
+            );
+        }
+        dispatch(authAction.loginThunk(form, loginSuccess, loginError))
+    }
     return (
         <View style={styles.container}>
             <ImageBackground
-                source={require('../assets/images/bgLogin.png')}
+                source={require('../../assets/images/bgLogin.png')}
                 resizeMode="cover"
                 style={styles.bg}>
                 <View style={styles.content}>
                     <Text style={styles.title}>Login</Text>
-                    <Input value={email} setState={setEmail} placeholder='Enter your email adress' isPassword={false} type='email-address' />
-                    <Input value={password} setState={setPassword} placeholder='Enter your password' isPassword={true} type='none' />
-                    <Text style={styles.forgot}>Forgot Password?</Text>
-                    <TouchableOpacity style={styles.createBtn} onPress={() => { navigation.navigate('Signup') }}>
-                        <Text style={styles.textCreate}>Login</Text>
+                    <Input value={form.email} handler={onChangeHandler} placeholder='Enter your email address' isPassword={false} text={'email'} />
+                    <Input value={form.password} handler={onChangeHandler} placeholder='Enter your password' isPassword={true} text={'password'} />
+                    <Text style={styles.forgot} onPress={()=>{navigation.navigate('Forgot')}}>Forgot Password?</Text>
+                    <TouchableOpacity style={styles.createBtn} onPress={handleSubmit}>
+                        {auth.isLoading ? <View style={styles.btnLoading}>
+                            <ActivityIndicator size='large' color='black' />
+                        </View> : <Text style={styles.textCreate}>Login</Text>}
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.googleBtn}>
                         <View style={styles.googleContainer}>
                             <Image source={google} />
-                            <Text style={styles.textGoogle}>Create with Google</Text>
+                            <Text style={styles.textGoogle}>Login with Google</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
