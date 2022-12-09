671e2bc2f257591ab44e91c88038ba5f6db80f30
rofa <zainrofa29@gmail.com>
2022-12-10 05:38:45 +0700

    set up redux

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import React from 'react';
+import store, { persistedStore } from './src/redux/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWithRedux = () => (
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistedStore}>
+            <App />
+        </PersistGate>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => AppWithRedux);
