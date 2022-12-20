ce739a73b99955634504f8619a6e27a956da6a1a
rofa <zainrofa29@gmail.com>
2022-12-20 11:29:38 +0700

    add conditional role

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -2,24 +2,34 @@ import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
 import React from 'react'
 import styles from "../styles/CardProduct";
 import { useNavigation } from '@react-navigation/native';
+import IconComunity from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useSelector } from 'react-redux';
 
 // import Sample from "../assets/images/product.png"
 
 
 const CardProduct = ({ img, name, price, id }) => {
     const navigation = useNavigation()
+    const role = useSelector(state => state.auth.userData.role)
+
     const costing = (price) => {
         return (
-          "IDR " +
-          parseFloat(price)
-            .toFixed()
-            .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
+            "IDR " +
+            parseFloat(price)
+                .toFixed()
+                .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
         );
     };
     return (
         <Pressable style={styles.card} onPress={() => { navigation.navigate("ProductDetail", id) }}>
+            {role === 'User' && <Pressable
+                style={styles.conPencl}
+                onPress={() => setModalVisible(true)}>
+                <IconComunity name={'pencil'} style={styles.pencil} size={20} />
+            </Pressable>}
+            
             <View style={styles.containerImage}>
-                <Image source={{uri:img}} style={styles.imageCard} />
+                <Image source={{ uri: img }} style={styles.imageCard} />
             </View>
             <View style={styles.containerTitle}>
                 <Text style={styles.cardTitle}>{name}</Text>
--- src/components/CardPromo.js
+++ src/components/CardPromo.js
@@ -1,15 +1,24 @@
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
 import React from 'react'
 import styles from "../styles/CardPromo";
+import IconComunity from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
 
 // import Sample from "../assets/images/product.png"
 
 
 const CardPromo = ({ img, name, code}) => {
     // const navigation = useNavigation()
+    const role = useSelector(state => state.auth.userData.role)
+
     return (
         <Pressable style={styles.card}>
+            {role === 'User' && <Pressable
+                style={styles.conPencl}
+                onPress={() => setModalVisible(true)}>
+                <IconComunity name={'pencil'} style={styles.pencil} size={20} />
+            </Pressable>}
             <View style={styles.containerImage}>
                 <Image source={{uri:img}} style={styles.imageCard} />
             </View>
